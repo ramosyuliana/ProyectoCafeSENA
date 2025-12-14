d63f5cbe2843dd9014d0f744b87a38fdaddf4833
ramosyuliana <ramosyuliana288@gmail.com>
2025-12-14 17:10:37 -0500

    Primer commit

---
+++ src/main/java/com/cafesena/cafesena/CafesenaApplication.java
@@ -0,0 +1,13 @@
+package com.cafesena.cafesena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafesenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafesenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cafesena/cafesena/CafesenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafesena.cafesena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafesenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
